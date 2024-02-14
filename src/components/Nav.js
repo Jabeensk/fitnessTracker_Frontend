7bfe6f8c68ab2fe13adfaf93e34890b306afad0f
Jabeen <jabeensyed@gmail.com>
2024-02-14 18:10:22 -0500

    styling the links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,73 +7,90 @@ import profileIcon from "../views/profilepage.jpg";
 import nutritionIcon from "../views/nutrition.jpg";
 import healthIcon from "../views/health.jpg";
 
-
 function Nav({ handleLogout }) {
   const logout = () => {
-    
     handleLogout();
   };
-  
+
   return (
     <nav>
-      
-      <div style={{ display: "flex", justifyContent: "center", marginBottom: "20px" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: "20px",
+        }}
+      >
         <img
           src={healthIcon}
           alt="health"
           style={{ height: "200px", width: "200px" }}
           className="health"
-          />
-          <nav className="navbar navbar-dark bg-primary">
-        <div className="container-fluid">
-          <img src={logo} alt="Logo" className="logo" />
-          <span className="navbar-brand mb-0 h1">
-            <h1>My Burn Buddy</h1>
-          </span>
-        </div>
-        
-      </nav>
+        />
+        <nav className="navbar navbar-dark bg-primary">
+          <div className="container-fluid">
+            <img src={logo} alt="Logo" className="logo" />
+            <span className="navbar-brand mb-0 h1">
+              <h1>My Burn Buddy</h1>
+            </span>
+          </div>
+        </nav>
       </div>
 
-
       <ul>
-       
-        <img
-          src={homeIcon}
-          alt="Home"
-          style={{ marginRight: "20px", height: "60px", width: "50px" }}
-        />
-        <Link to="/">Home</Link>
+        <Link to="/" style={{ marginRight: "20px", marginLeft: "-10px" }}>
+          <img
+            src={homeIcon}
+            alt="Home"
+            style={{ height: "60px", width: "50px" }}
+          />
+          Home
+        </Link>
 
-        <img
-          src={exerciseIcon}
-          alt="Workouts"
-          style={{ marginRight: "20px", height: "60px", width: "50px" }}
-        />
-        <Link to="/workouts">Workouts</Link>
 
-        <img
-          src={profileIcon}
-          alt="Profile"
-          style={{ marginRight: "20px", height: "60px", width: "50px" }}
-        />
-        <Link to="/profile">Profile</Link>
+        <Link
+          to="/workouts"
+          style={{ marginRight: "20px", marginLeft: "-10px" }}
+        >
+          <img
+            src={exerciseIcon}
+            alt="Workouts"
+            style={{ height: "60px", width: "50px" }}
+          />
+          Workouts
+        </Link>
+        <Link
+          to="/nutrition"
+          style={{ marginRight: "20px", marginLeft: "-10px" }}
+        >
+          <img
+            src={nutritionIcon}
+            alt="Nutrition"
+            style={{ height: "60px", width: "50px" }}
+          />
+          Nutrition
+        </Link>
 
-        <img
-          src={nutritionIcon}
-          alt="Nutrition"
-          style={{ marginRight: "20px", height: "60px", width: "50px" }}
-         
-        />
-        <Link to="/nutrition">Nutrition</Link>
+        <Link
+          to="/profile"
+          style={{ marginRight: "20px", marginLeft: "-10px" }}
+        >
+          <img
+            src={profileIcon}
+            alt="Profile"
+            style={{ height: "60px", width: "50px" }}
+          />
+          Profile
+        </Link>
 
-        <img
-          src={logo}
-          alt="Nutrition"
-          style={{ marginRight: "20px", height: "60px", width: "50px" }}
-         
-        />
-        <Link to="/about">About</Link>
+        <Link to="/about" style={{ marginRight: "20px", marginLeft: "-10px" }}>
+          <img
+            src={logo}
+            alt="About"
+            style={{ height: "60px", width: "50px" }}
+          />
+          About
+        </Link>
 
         <button onClick={logout} className="custom-button">
           Logout
